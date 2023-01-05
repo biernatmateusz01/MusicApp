b1200b115185361dd6015db36fd56d2ef0fcf520
biernatmateusz01 <biernat521@gmai.com>
2023-01-05 12:50:40 +0100

    bug

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <div class="fixed top-0 flex left-0 h-screen justify-center items-center w-screen bg-black z-[60]">
+      <img src=""/>
+      <span class="text-white text-3xl">
+      Przerwa Techniczna
+  <br>
+  Zmieniamy się dla Ciebie daj nam chwilkę czasu.
+      </span>
+    </div>
     <HomeView/>
   </div>
 </template>
